dac68ab1e04fa5f37cd38b33d43c1caebb86e009
drdevlin <drdevlin@fastmail.com>
2021-01-23 13:32:18 -0500

    Make App render some components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import Now from '../Now/Now';
+import Extreme from '../Extreme/Extreme';
+import Precip from '../Precip/Precip';
+import Wind from '../Wind/Wind';
+import Tomorrow from '../Tomorrow/Tomorrow';
+
 function App() {
   return (
     <main className="App">
-      
+      <Now />
+      <Extreme />
+      <Precip />
+      <Wind />
+      <Tomorrow />
     </main>
   );
 }
