7b136f86665c82bb83feaacf6adf0062dbf09f3d
drdevlin <drdevlin@fastmail.com>
2021-01-23 13:56:37 -0500

    Add some data to store for development

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,32 @@
 import { createStore } from 'redux';
 
-const initialState = {};
+const initialState = {
+  app: {
+    condition: 'nice'
+  },
+  now: {
+    temp: 0,
+    type: 'normal'
+  },
+  extreme: {
+    temp: 0,
+    type: 'normal',
+    time: Date.now()
+  },
+  precip: {
+    time: Date.now(),
+    pops: []
+  },
+  wind: {
+    speeds: []
+  },
+  tomorrow: {
+    condition: 'nice',
+    temp: 0
+  },
+  fetchStatus: 'idle',
+  error: null
+};
 
 const reducer = (state, action) => {
   return state;
