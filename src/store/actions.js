7fc51466560a5804f9c3f5769e4828d42933ddeb
drdevlin <drdevlin@fastmail.com>
2021-01-28 16:03:25 -0500

    Redux: create actions and add reducers to store

---
+++ src/store/actions.js
@@ -0,0 +1,20 @@
+export const updateApp = (app) => {
+  return { type: 'UPDATE_APP', app };
+}
+export const updateExtreme = (extreme) => {
+  return { type: 'UPDATE_EXTREME', extreme };
+}
+export const updateNow = (now) => {
+  return { type: 'UPDATE_NOW', now };
+}
+export const updatePrecip = (precip) => {
+  return { type: 'UPDATE_PRECIP', precip };
+}
+export const updateTomorrow = (tomorrow) => {
+  return { type: 'UPDATE_TOMORROW', tomorrow };
+}
+export const updateWind = (wind) => {
+  return { type: 'UPDATE_WIND', wind };
+}
+
+
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import * as is from './actions';
 
 const initialState = {
   app: {
@@ -29,7 +30,22 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case is.updateApp().type:
+      return { ...state, app: action.app };
+    case is.updateExtreme().type:
+      return { ...state, extreme: action.extreme };
+    case is.updateNow().type:
+      return { ...state, now: action.now };
+    case is.updatePrecip().type:
+      return { ...state, precip: action.precip };
+    case is.updateTomorrow().type:
+      return { ...state, tomorrow: action.tomorrow };
+    case is.updateWind().type:
+      return { ...state, wind: action.wind };
+    default:
+      return state;
+  }
 };
 
 const store = createStore(reducer, initialState);
